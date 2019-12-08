b4f3d4c3e8d9d5db4630d9b14b65549680041b9c
David <desmaisons_david@hotmail.com>
2019-12-08 13:50:52 -0300

    Adding boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = (_, argv) => ({
+  entry: (argv.mode === 'development')  ? `./example/index.js` : './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    liveReload: true,
+    port: 8050
+  },
+  plugins: [
+    new webpack.SourceMapDevToolPlugin({   
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template:  `./public/index.html`
+    })
+  ],
+  output: {
+    libraryTarget: 'umd',
+    library: '{mpm-name-here}',
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+});
