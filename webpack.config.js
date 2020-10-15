b66387241fa202564a0fb33eb6a3b5f20adc142a
Desmaisons <desmaisons_david@hotmail.com>
2020-10-15 10:14:41 -0300

    Adding plugin transform runtime

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,8 @@ module.exports = (_, argv) => ({
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime']
           }
         }
       }
